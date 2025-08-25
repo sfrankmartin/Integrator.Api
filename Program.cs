29ddec235e009163bfe4a7d1c9cab358771e0a48
S. Frank Martin <sfmartin85@gmail.com>
2025-08-25 14:29:49 +1000

    feat: config Swagger to support action descriptions and param documentation

--- Program.cs
+++ Program.cs
@@ -12,16 +12,27 @@ public class Program
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = System.IO.Path.Combine(AppContext.BaseDirectory, xmlFile);
+            if (System.IO.File.Exists(xmlPath))
+            {
+                options.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+            }
+        });
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
         }
+
+        // PLEASE NOTE: Since this is a demonstration app, Swagger is exposed in production.
+        // Normally this would only be available in development, enclosed in the `if` check above.
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
         app.UseAuthorization();
 
